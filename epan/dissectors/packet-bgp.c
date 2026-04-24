oXoi/wireshark

commit 85ff46c76b2a6a4b94094e4f20dfa66b43086182

BGP: fix reader_offset handling for Source Address Length
Fixes: f10ca976ab1e ("BGP: Add Source Address to Type 1 Session Transformed Route")

Signed-off-by: Yuya Kusakabe <y-kusakabe@bbsakura.net>

epan/dissectors/packet-bgp.c
@@ -7579,9 +7579,8 @@ static int decode_bgp_mup_nlri_type1_st_route(proto_tree *tree, tvbuff_t *tvb, i
             return -1;
         }
         source_address_length = tvb_get_uint8(tvb, reader_offset); // should be zero or multiple of 8
-        if (source_address_length==0) {
-            reader_offset++;
-        } else if (source_address_length==32) {
+        reader_offset++;
+        if (source_address_length==32) {
             proto_tree_add_item(arch_spec_tree, hf_bgp_mup_nlri_3gpp_5g_source_ip_addr, tvb, reader_offset, 4, ENC_BIG_ENDIAN);
             reader_offset += 4;
         } else if (source_address_length==128) {

commit 5bda590d5598b8a4e0c4ade8dc4cef585db0b619

BGP: Accept zero-length Source Address in MUP Type 1 ST route
A Source Address Length of 0 means the Source Address is not carried
within the NLRI (draft-ietf-bess-mup-safi-00 Section 3.1.3.1).

Fixes: 85ff46c76b2a ("BGP: fix reader_offset handling for Source Address Length")

Signed-off-by: Yuya Kusakabe <yuya.kusakabe@gmail.com>

epan/dissectors/packet-bgp.c
@@ -7560,7 +7560,8 @@ static int decode_bgp_mup_nlri_type1_st_route(proto_tree *tree, tvbuff_t *tvb, u
             +-----------------------------------+
         */
         endpoint_address_length = tvb_get_uint8(tvb, reader_offset+5); // should be multiple of 8
-        arch_spec_byte = 7 + endpoint_address_length/8;
+        source_address_length = tvb_get_uint8(tvb, reader_offset + 6 + endpoint_address_length/8); // should be zero or multiple of 8
+        arch_spec_byte = 7 + endpoint_address_length/8 + source_address_length/8;
 
         arch_spec_item = proto_tree_add_item(tree, hf_bgp_mup_nlri_3gpp_5g_type1_st_route, tvb, reader_offset, arch_spec_byte, ENC_NA);
         arch_spec_tree = proto_item_add_subtree(arch_spec_item, ett_bgp_mup_nlri_3gpp_5g_type1_st_route);
@@ -7585,9 +7586,11 @@ static int decode_bgp_mup_nlri_type1_st_route(proto_tree *tree, tvbuff_t *tvb, u
                                    "Invalid length (%u) of Endpoint Address Length", endpoint_address_length);
             return -1;
         }
-        source_address_length = tvb_get_uint8(tvb, reader_offset); // should be zero or multiple of 8
+        proto_tree_add_item(arch_spec_tree, hf_bgp_mup_nlri_3gpp_5g_source_addr_len, tvb, reader_offset, 1, ENC_BIG_ENDIAN);
         reader_offset++;
-        if (source_address_length==32) {
+        if (source_address_length==0) {
+            /* Source Address Length of 0 means the Source Address is not carried within the NLRI. */
+        } else if (source_address_length==32) {
             proto_tree_add_item(arch_spec_tree, hf_bgp_mup_nlri_3gpp_5g_source_ip_addr, tvb, reader_offset, 4, ENC_BIG_ENDIAN);
             reader_offset += 4;
         } else if (source_address_length==128) {